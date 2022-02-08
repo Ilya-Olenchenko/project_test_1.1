05d4e1f4d4f4d40ac035a721eda2ee73442bf656
Ілля <olenchenko.ilya@ukr.net>
2022-02-08 22:46:42 +0200

    "Неправельный" splash screen.

--- app/src/main/java/com/example/project_test_11/MainActivity.java
+++ app/src/main/java/com/example/project_test_11/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.project_test_11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                finish();
+            }
+        },5000);
     }
 }
