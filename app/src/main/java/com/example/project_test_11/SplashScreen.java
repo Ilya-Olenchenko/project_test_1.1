dc201abfa9ac9a7e81e1afda339f63b6c1e36f67
Ілля <olenchenko.ilya@ukr.net>
2022-02-09 00:32:30 +0200

    Добавлен правильный SplashScreen и + сделана задержка в 5 секунд.

---
+++ app/src/main/java/com/example/project_test_11/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.example.project_test_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
