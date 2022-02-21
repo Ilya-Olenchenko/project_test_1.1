416327aa3ee1ca3bbe6b3d9b2ad2c4ef33384f6a
Ілля <olenchenko.ilya@ukr.net>
2022-02-21 20:00:02 +0200

    Правки отображение белого на иконках в нижней панели

--- app/src/main/java/com/example/project_test_11/HomeActivity.java
+++ app/src/main/java/com/example/project_test_11/HomeActivity.java
@@ -2,56 +2,43 @@ package com.example.project_test_11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.content.res.Resources;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public void setColor(int obj, int color){
+        final ImageView imageView = findViewById(obj);
+        imageView.setColorFilter(imageView.getContext().getResources().getColor(color), PorterDuff.Mode.SRC_ATOP);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
 
+        setColor(R.id.imageView_home_icon, R.color.white);
+    }
 
 
     public void burger_click(View view) {
 
-
-        //burger.setAnimation(null);
     }
 
     public void home_click(View view) {
-        Resources resources = getResources();
-        int textColor = resources.getColor(R.color.white);
-        final ImageView home = findViewById(R.id.imageView_home_icon);
-        home.setColorFilter(textColor);
 
     }
 
     public void statistic_click(View view) {
-        Resources resources = getResources();
-        int textColor = resources.getColor(R.color.white);
-        final ImageView statistic = findViewById(R.id.imageView_statistic_icon);
-        statistic.setColorFilter(textColor);
-    }
 
+    }
     public void dream_click(View view) {
-        Resources resources = getResources();
-        int textColor = resources.getColor(R.color.white);
-        final ImageView dream = findViewById(R.id.imageView_dream_icon);
-        dream.setColorFilter(textColor);
+
     }
 
     public void music_click(View view) {
-        Resources resources = getResources();
-        int textColor = resources.getColor(R.color.white);
-        final ImageView music = findViewById(R.id.imageView_music_icon);
-        music.setColorFilter(textColor);
+
     }
 }
